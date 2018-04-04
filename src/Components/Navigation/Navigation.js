bfb63a6b7f9753a2c8dd25297befc025a8d3275a
alexr101 <alexonezero@outlook.com>
2018-04-04 16:50:13 -0400

    first few basic lessons

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './Navigation.css';
+
+class Navigation extends Component {
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return (
+      <div>
+        <nav>
+          
+        </nav>
+
+      </div>
+
+    )
+  }
+}
---
+++ src/Components/Person/Person.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const person = () => {
+  return  <div>
+            <h1>Person Component</h1>
+          </div>
+};
+
+export default person;
