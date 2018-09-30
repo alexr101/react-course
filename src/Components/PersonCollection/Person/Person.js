6d3fed3eacd3857e69562d5422f0a1515a8007f9
alexr101 <alex_r_101@hotmail.com>
2018-09-30 17:45:37 -0400

    tranform Person & PersonCollection into React Components

--- src/Components/PersonCollection/Person/Person.js
+++ src/Components/PersonCollection/Person/Person.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Button from '../../Button/Button';
 import classes from './Person.css'
 
-
-const person = (props) => {
-
-  return  <div className={classes.Person} onClick={props.click}>
-            <h2>Profile Card</h2>
-            <h3>Name: {props.name}</h3>
-            <h3>Age: {props.age}</h3>
-            <p>{props.children}</p>
-            <div className={classes.formInputs}>
-              <label htmlFor="name">Name</label>
-              <input id="name" onChange={(e) => props.onNameChangedHandler(e, props.id)} placeholder={props.name}/>
+class Person extends Component {
+  render() {
+    return  <div className={classes.Person} onClick={this.props.click}>
+              <h2>Profile Card</h2>
+              <h3>Name: {this.props.name}</h3>
+              <h3>Age: {this.props.age}</h3>
+              <p>{this.props.children}</p>
+              <div className={classes.formInputs}>
+                <label htmlFor="name">Name</label>
+                <input id="name" onChange={(e) => this.props.onNameChangedHandler(e, this.props.id)} placeholder={this.props.name}/>
+              </div>
+              <div className={classes.formInputs}>
+                <label htmlFor="age">Age</label>
+                <input id="age" onChange={(e) => this.props.onAgeChangedHandler(e, this.props.id)} placeholder={this.props.age}/>
+              </div>
+              <Button click={this.props.updatePersonHandler}>Update</Button>
+              <Button click={() => this.props.removePerson(this.props.id)} buttonType="red">Remove</Button>
             </div>
-            <div className={classes.formInputs}>
-              <label htmlFor="age">Age</label>
-              <input id="age" onChange={(e) => props.onAgeChangedHandler(e, props.id)} placeholder={props.age}/>
-            </div>
-            <Button click={props.updatePersonHandler}>Update</Button>
-            <Button click={() => props.removePerson(props.id)} buttonType="red">Remove</Button>
-          </div>
-};
+  }
+}
 
-export default person;
+
+export default Person;
--- src/Components/PersonCollection/PersonCollection.js
+++ src/Components/PersonCollection/PersonCollection.js
@@ -1,29 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ErrorBoundary from '../../ErrorBoundary/ErrorBoundary';
 import Person from './Person/Person';
 
-const PersonCollection = (props) => {
-    // const rnd = Math.random();
-    // if(rnd > .7) {
-    //   throw new Error('something went wrong');
-    // }
-    console.log(props)
-    return props.people.map((person, i) => {
-      return (
-        // <ErrorBoundary key={i}>
-          <Person  
-          key={i}
-            id = {i}
-            name={person.name} 
-            age={person.age}
-            onNameChangedHandler = {props.onNameChangedHandler}
-            onAgeChangedHandler = {props.onAgeChangedHandler}
-            updatePersonHandler = {props.updatePersonHandler}
-            removePerson = {props.removePerson}
-            ></Person>
-        //   </ErrorBoundary>   
-      );
-    })
+class PersonCollection extends Component {
+    render() {
+        return this.props.people.map((person, i) => {
+            return (
+              // <ErrorBoundary key={i}>
+                <Person  
+                key={i}
+                  id = {i}
+                  name={person.name} 
+                  age={person.age}
+                  onNameChangedHandler = {this.props.onNameChangedHandler}
+                  onAgeChangedHandler = {this.props.onAgeChangedHandler}
+                  updatePersonHandler = {this.props.updatePersonHandler}
+                  removePerson = {this.props.removePerson}
+                  ></Person>
+              //   </ErrorBoundary>   
+            );
+          })
+    }
 }
 
 export default PersonCollection;
