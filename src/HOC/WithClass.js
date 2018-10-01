62c13ebbe9084f1301fed81ce21c1fa2764798db
alexr101 <alex_r_101@hotmail.com>
2018-09-30 23:51:37 -0400

    add class factory to WithClass Higher Order Component

--- src/HOC/WithClass.js
+++ src/HOC/WithClass.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 // Wrapper class to wrap exports
-const WithClass = (WrappedComponent, className) => {
-    return (props) => (
-        <div className={className}>
-            <WrappedComponent {...props}/>
-        </div>
-    )
-}
+// const WithClass = (WrappedComponent, className) => {
+//     return (props) => (
+//         <div className={className}>
+//             <WrappedComponent {...props}/>
+//         </div>
+//     )
+// }
 
+// or factory method if we need a stateful component
+
+const WithClass = (WrappedComponent, className) => {
+    return class extends Component {
+        render() {
+            return (
+                <div className={className}>
+                    <WrappedComponent {...this.props}/>
+                </div>
+            )
+        }
+    }
+}
 export default WithClass;
